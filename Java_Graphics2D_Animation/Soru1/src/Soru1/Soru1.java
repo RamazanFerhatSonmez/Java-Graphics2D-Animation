0c42742c9e48dba764701453bb5d6aef05c3020d
RamazanFerhat <32498472+RamazanFerhatSonmez@users.noreply.github.com>
2019-01-17 17:19:10 +0300

    code editing
    code editing

--- Java_Graphics2D_Animation/Soru1/src/Soru1/Soru1.java
+++ Java_Graphics2D_Animation/Soru1/src/Soru1/Soru1.java
@@ -15,38 +15,26 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 class Surface extends JPanel implements ActionListener {
-
     private final int DELAY = 200;
     private int adim = 3;
-
     private Timer timer;
     int sayac = 0;
-
     public Surface() {
-
-        initTimer();
-         
+        initTimer();     
     }
-
     private void initTimer() {
-
         timer = new Timer(DELAY, this);
         timer.start();
     }
-    
-    public Timer getTimer() {
-        
+    public Timer getTimer() {   
         return timer;
     }
-
     private void doDrawing(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
         Random c = new Random();
         int w = getWidth();
         int h = getHeight();
-        
-
         Random r = new Random();
         BasicStroke bs3 = new BasicStroke(3, BasicStroke.CAP_SQUARE,
         BasicStroke.JOIN_BEVEL);
@@ -61,57 +49,33 @@ class Surface extends JPanel implements ActionListener {
             timer.stop();
             super.paintComponent(g);
         }
-        
     }
-
     @Override
     public void paintComponent(Graphics g) {
-        
-        super.paintComponent(g);
         doDrawing(g);
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         repaint();
     }
 }
-
 public class Soru1 extends JFrame {
 
     public Soru1() {
 
         initUI();
     }
-
     private void initUI() {
 
         final Surface surface = new Surface();
         add(surface);
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                Timer timer = surface.getTimer();
-                timer.stop();
-            }
-        });
-
         setTitle("1.soru");
         setSize(800, 800);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
     public static void main(String[] args) {
-
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-
             	Soru1 ex = new Soru1();
                 ex.setVisible(true);
-            }
-        });
     }
 }
